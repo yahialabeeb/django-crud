aab9b068799829bbf70fec83ddd0cfcd579b184c
Yahialabeeb <yahia.labeeb@gmail.com>
2021-12-08 15:23:59 +0200

    first commit

---
+++ snacks/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+class Snack(models.Model):
+    title = models.CharField(max_length= 255)
+    description = models.TextField()
+    purchaser = models.ForeignKey(get_user_model(), on_delete= models.CASCADE)
+
+    def get_absolute_url(self):
+        return reverse('thing_detail', args=[self.id])
